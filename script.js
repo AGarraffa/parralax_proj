044115a2921eb834edf37f0ddd352348ec904d29
Alfred Garraffa <agarraffa@gmail.com>
2023-03-29 11:37:09 -0600

    layers successfully drawn

--- script.js
+++ script.js
@@ -17,24 +17,56 @@ backgroundLayer4.src= '../assets/background_layer/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src= '../assets/background_layer/layer-5.png';
 
-// horizontal position of the background images=
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        // used for doubling the image to aid in infinite scrolling
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+
+    // this makes sure that the image redraws once it reaches the end of the image
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width){
+            this.x2= this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    // this draws the image twice so that you can infinitely scroll
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.1);
+const layer2 = new Layer(backgroundLayer2, 0.25);
+const layer3 = new Layer(backgroundLayer3, 0.15);
+const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const backgrounds = [layer1, layer2, layer3, layer4, layer5]
 
 function animateBG() {
 
     // clears the image for each animation cycle so it doesn't continually draw the image
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    ctx.drawImage(backgroundLayer4, x, 0);
-    // drawing the image again to help with the scrolling
-    ctx.drawImage(backgroundLayer4, x2, 0);
 
-    // the image will scroll off the screen. Resetting the x value get's the image back in to frame. 2400 is the pixel width of this particular image so you just need to know that based on your assets.
-    if (x < -2400) x = 2400;
-    // changing the rate at which x decreases will change how fast the image scrolls
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400;
-    else x2 -= gameSpeed;
+    backgrounds.forEach(object => {
+        object.update();
+        object.draw();
+    })
+
     requestAnimationFrame(animateBG);
 };
 
